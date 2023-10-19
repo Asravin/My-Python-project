5b66bfa51838221e467add6bba610d2322a2c670
Asravin <maisweedfell@.com>
2023-10-19 12:54:56 +0400

    Объединение строк

---
+++ My project/project_2.py
@@ -0,0 +1,9 @@
+s1 = "Lorem ipsum dolor sit amet, consectetur adiposcing elit."
+s2 = "Proin porta, libero sit amet portttor tempor, sapien diam vulputate"
+s3 = "libero, quis porttitor nulla leo auctor felis."
+
+# s4 = s1 + s2 + s3
+# print(s4)
+
+print("\n\n\n")
+print(s1 + s2 + s3)
