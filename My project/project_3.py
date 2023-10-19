1b8b97affda3fa25cfb1cad9b2465a076f7a6bf3
Asravin <maisweedfell@.com>
2023-10-19 15:03:07 +0400

    Изменение строк в файле

---
+++ My project/project_3.py
@@ -0,0 +1,11 @@
+test = "привет"
+print(test.upper())
+print(test)
+test = test.upper()
+print(test)
+print(test.lower())
+test = test.swapcase()
+print(test.capitalize())
+
+test = "У Саши есть 10 арбузов"
+print(test.replace("арбузов", "яблок"))
