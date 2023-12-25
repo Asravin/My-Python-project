9cb4baeeabfb6041d225896d54fc32c2eedee5c4
Asravin <maisweedfell@gmail.com>
2023-12-25 11:17:39 +0400

    Присвоение новых значений списку

---
+++ My project/list_6.py
@@ -0,0 +1,10 @@
+cars = ["audi", "vw", "lexus", "gtr", "m5"]
+japan_cars = ["toyota", "nissan"]
+cars += japan_cars
+print(cars)
+
+# cars[0] = "bentley"
+# print(cars)
+
+cars[1 : 2] = ["bentley", "bmw"]
+print(cars)
