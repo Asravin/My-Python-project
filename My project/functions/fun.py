055aa353e7766e4de369c48221e8b01866577fef
Asravin <maisweedfell@gmail.com>
2023-12-29 13:55:34 +0400

    Глобальные переменные

--- My project/functions/fun.py
+++ My project/functions/fun.py
@@ -1,4 +1,14 @@
-def fun():
-    return 1, 2, 3
-a, b, c = fun()
-print(a, b, c)
+def fun1():
+    global res
+    res = 50
+    print(res)
+
+def fun2():
+    global res
+    print(res)
+
+res = 30
+print(res)
+fun1()
+fun2()
+print(res)
