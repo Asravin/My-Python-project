a1209c8e229549a91d8e48df06654dffe48b53c2
Asravin <maisweedfell@gmail.com>
2024-01-16 14:46:11 +0400

    Чтение всего файла

--- My project/work in file/read_text.py
+++ My project/work in file/read_text.py
@@ -1,15 +1,29 @@
 print("** Открытие и закрытие файла")
-txt = open(r"C:\Users\Asravin\Desktop\Python Project\My project\work in file\read_text.py", encoding="utf-8")
+txt = open(r"My project\work in file\text.txt", encoding="utf-8")
 txt.close()
 
-
+print()
 
 print("** Посимвольное чтение файла")
-txt = open(r"C:\Users\Asravin\Desktop\Python Project\My project\work in file\read_text.py", encoding="utf-8")
+txt = open(r"My project\work in file\text.txt")
 print(txt.read(1))
 print(txt.read(2))
-print(txt.read(3))
+print(txt.read(6))
 txt.close()
 
+print()
 
- 
+print("** Посимвольное чтение файла с указанием кодировки")
+txt = open(r"My project\work in file\text.txt", encoding="utf-8")
+print(txt.read(1))
+print(txt.read(2))
+print(txt.read(6))
+txt.close()
+
+print()
+
+print("** Чтение всего файла")
+txt = open(r"My project\work in file\text.txt", encoding="utf-8")
+content = txt.read()
+print(content)
+txt.close()
