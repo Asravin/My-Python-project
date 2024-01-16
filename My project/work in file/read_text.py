6effcce6495ca2e2929330f959e78264c512b413
Asravin <maisweedfell@gmail.com>
2024-01-16 14:51:13 +0400

    Чтение построчно

--- My project/work in file/read_text.py
+++ My project/work in file/read_text.py
@@ -27,3 +27,21 @@ txt = open(r"My project\work in file\text.txt", encoding="utf-8")
 content = txt.read()
 print(content)
 txt.close()
+
+print()
+
+print("** Читаем строку из файла посимвольно")
+txt = open(r"My project\work in file\text.txt", encoding="utf-8")
+print(txt.readline(1))
+print(txt.readline(5))
+txt.close()
+
+print()
+
+print("** Читаем строку из файла полностью")
+txt = open(r"My project\work in file\text.txt", encoding="utf-8")
+print(txt.readline())          # Строка 1
+print(txt.readline())          # Строка 2
+txt.close()
+
+print()
