f7a4904d4c1e9116d25345bd923d38de38f2b1e3
Asravin <maisweedfell@gmail.com>
2024-01-22 12:02:48 +0400

    Консервация списка

---
+++ My project/work in file/closing_down_read.py
@@ -0,0 +1,9 @@
+import pickle, shelve
+
+first_name = ["Оля", "Вася", "Коля"]
+last_name = ["Петрова", "Пупкин", "Смирнов"]
+
+datafile = open(r"My project\work in file\names.dat", "wb")
+pickle.dump(first_name, datafile)
+pickle.dump(last_name, datafile)
+datafile.close()
--- My project/work in file/write_list_0.py
+++ My project/work in file/write_list_0.py
@@ -1,14 +1,14 @@
 text = "Первая строка"
+import gzip 
+import bz2 
 
-
-
-import gzip            
+    
+           
 with gzip.open(r"My project\work in file\list.gz", "wt") as f:
     f.write(text)
     
 
-
-import bz2         
+   
 with bz2.open(r"My project\work in file\list.bz2", "wt") as f:
     f.write(text)
     
