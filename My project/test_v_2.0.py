ec4ecea9edc348c1f59bc7a3f83b0f912463aa96
Asravin <maisweedfell@gmail.com>
2024-01-25 15:16:54 +0400

    Добавленно чтение из текстового файла

---
+++ My project/test_v_2.0.py
@@ -0,0 +1,22 @@
+import pickle
+mark = 0
+print("*" * 10, "Тесты 2.0", "*" * 10)
+try:
+     datafile = open(r"test.dat", "rb")
+except:
+     print("Ошибка при загрузке вопросов!")
+else:
+     questions = pickle.load(datafile)
+     answers = pickle.load(datafile)
+     datafile.close()
+     n = len(answers)
+     i = 0
+     for i in range(0, n):
+          print(questions[i])
+          a = int(input("Ваш ответ: "))
+          if a == answers[i]:
+               mark += 1
+               print("Правильно!")
+          else:
+               print("Неправильно :(")
+     print("Вы правильно ответили на ", mark, "вопросов из", n)
