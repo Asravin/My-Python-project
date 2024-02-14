32bc3a871be5d0c82afb3f54d1e03f1e51137c61
Asravin <maisweedfell@gmail.com>
2024-02-14 13:16:36 +0200

    Программа Виртуальная кошка

--- My project/OOP/class_cat.py
+++ My project/OOP/class_cat.py
@@ -1,9 +1,19 @@
 class Cat(object):
     """Виртуальная кошка"""
+    total = 0
+    @staticmethod
+    
+    def count():
+        print("Всего кошек: ", Cat.total)
     
     def __init__(self, name):
         print("Родилась новая кошка!")
         self.name = name
+        Cat.total += 1
+        
+    def __str__(self):
+        res = "Объект класса Cat\n name: " + self.name
+        return res
         
     def talk(self):
         print(self.name, ": Мяу")
