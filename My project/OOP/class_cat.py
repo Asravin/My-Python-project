79561a67e66a4916584b1188a1e997083fe0a6a0
Asravin <maisweedfell@gmail.com>
2024-02-15 17:31:17 +0200

    Виртуальная кошка версии 2.0

--- My project/OOP/class_cat.py
+++ My project/OOP/class_cat.py
@@ -10,10 +10,23 @@ class Cat(object):
         print("Родилась новая кошка!")
         self.name = name
         Cat.total += 1
+        self.__w = 1
         
     def __str__(self):
-        res = "Объект класса Cat\n name: " + self.name
+        res = "Объект класса Cat\n name: " + self.name + "/nBec:" + str(self.__w)
         return res
         
     def talk(self):
         print(self.name, ": Мяу")
+    
+    @property
+    def name(self):
+        return self.__name
+    @name.setter
+    
+    def name(self, new_name):
+        if new_name == "":
+            print("Укажите имя кошки!")
+        else:
+            self.__name = new_name
+            print("Новое имя: ", self.__name)
