7b5ab6df88e19385e3fd08657f979452fd8651a4
Asravin <maisweedfell@gmail.com>
2024-02-20 13:04:05 +0200

    Создание простого графического интерфейса с вызовом кнопки в консоль

---
+++ My project/GUI/hello.py
@@ -0,0 +1,13 @@
+from tkinter import*
+def hello(event):
+    print("Hello World!")
+    
+root = Tk()
+but1 = Button(root)
+but1["text"] = "Привет"
+but1.bind("<Button-1>", hello)
+
+root.title("Заголовок окна программы")
+root.geometry("300x250")
+but1.pack()
+root.mainloop()
