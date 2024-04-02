c12d84c1dbf04f86efc6a0adc809741989e71655
Asravin <maisweedfell@gmail.com>
2024-04-02 10:24:51 +0300

    Игровое поле созданно

--- My project/game/Snake/main.py
+++ My project/game/Snake/main.py
@@ -1,10 +1,16 @@
 from tkinter import *
+
+WIDTH = 800
+HEIGHT = 600
+SEG_SIZE = 20
+IN_GAME = True
+
+
 root = Tk()
 root.title("Змейка")
 
 
-
-
-
-
+c = Canvas(root, width=WIDTH, height=HEIGHT, bg="#005500")
+c.grid()
+c.focus_set()
 root.mainloop()
